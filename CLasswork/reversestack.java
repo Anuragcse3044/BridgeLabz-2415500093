130761dbd2d3c5152ba460bf43bdf3d84ff33db2
Anuragcse3044 <sy95364175@gmail.com>
2026-03-19 09:47:02 +0530

    Update reversestack.java

--- CLasswork/reversestack.java
+++ CLasswork/reversestack.java
@@ -11,35 +11,28 @@ public class reversestack {
         
         T top = stack.pop();
 
-          
-        // Recursively reverse the remaining stack
+        
         reverseStack(stack);
         
-        // Insert the popped element at the bottom
         insertAtBottom(stack, top);
     }
     
-    // Helper method to insert an element at the bottom of the stack
     public static <T> void insertAtBottom(Stack<T> stack, T element) {
         if (stack.isEmpty()) {
             stack.push(element);
             return;
         }
         
-        // Pop the top element
         T top = stack.pop();
         
-        // Recursively insert element at bottom
         insertAtBottom(stack, element);
         
-        // Push the popped element back
         stack.push(top);
     }
     
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
         
-        // Create a sample stack: 1, 2, 3, 4, 5 (5 is at top)
         stack.push(1);
         stack.push(2);
         stack.push(3);
